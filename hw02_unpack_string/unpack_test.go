117007a65251de072bbf5064db74f71e88fca6e7
s.gavrilchik <s.gavrilchik@fzlabs.ru>
2026-06-12 17:07:51 +0300

    HW2 is completed

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -19,14 +19,14 @@ func TestUnpack(t *testing.T) {
 		{input: "🙃0", expected: ""},
 		{input: "aaф0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
 	}
 
 	for _, tc := range tests {
-		tc := tc
+		// tc := tc       -убрал так используется go 1.22 (вызывает ошибку)
 		t.Run(tc.input, func(t *testing.T) {
 			result, err := Unpack(tc.input)
 			require.NoError(t, err)
@@ -36,9 +36,9 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", `abc\`} // добавлен новый кейс
 	for _, tc := range invalidStrings {
-		tc := tc
+		// tc := tc         -убрал так используется go 1.22 (вызывает ошибку)
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
