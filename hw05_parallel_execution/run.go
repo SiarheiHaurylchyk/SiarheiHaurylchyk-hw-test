b0f98ecf4baa44f889b6ba664c36d9e30ff5f62e
s.gavrilchik <s.gavrilchik@fzlabs.ru>
2026-07-06 10:51:04 +0300

    HW5 fix

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -15,40 +15,53 @@ func Run(tasks []Task, n, m int) error {
 	if len(tasks) == 0 {
 		return nil
 	}
+	if m < 0 {
+		m = 0
+	}
 
 	taskChan := make(chan Task)
 	var errCount atomic.Int32
 	wg := sync.WaitGroup{}
+	done := make(chan struct{})
 
 	for i := 0; i < n; i++ {
 		wg.Add(1)
-		go worker(taskChan, &errCount, m, &wg)
+		go worker(taskChan, &errCount, m, &wg, done)
 	}
 
 	for _, task := range tasks {
-		if m > 0 && errCount.Load() >= int32(m) {
+		if m > 0 && int(errCount.Load()) >= m {
 			break
 		}
-		taskChan <- task
+		select {
+		case taskChan <- task:
+		case <-done:
+			break
+		}
 	}
 
 	close(taskChan)
 	wg.Wait()
 
-	if m > 0 && errCount.Load() >= int32(m) {
+	if m > 0 && int(errCount.Load()) >= m {
 		return ErrErrorsLimitExceeded
 	}
 	return nil
 }
 
-func worker(taskChan <-chan Task, errCount *atomic.Int32, m int, wg *sync.WaitGroup) {
+func worker(taskChan <-chan Task, errCount *atomic.Int32, m int, wg *sync.WaitGroup, done chan struct{}) {
 	defer wg.Done()
 
 	for task := range taskChan {
 		err := task()
-
 		if err != nil && m > 0 {
-			errCount.Add(1)
+			if int(errCount.Add(1)) >= m {
+				select {
+				case done <- struct{}{}:
+				default:
+				}
+				return
+			}
 		}
 	}
 }
